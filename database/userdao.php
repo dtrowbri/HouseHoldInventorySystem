1f87cc2414f79d9249a781b28ea3996fdd7381fe
dtrowbri <dtrowbridg@my.gcu.edu>
2021-10-24 21:27:22 -0700

    Created UserDAO

---
+++ database/userdao.php
@@ -0,0 +1,26 @@
+<?php
+//require_once './database.php';
+//require_once '../business/models/user.php';
+class UserDAO {
+    
+    function addUser(?User $user, $conn){
+        $query = "insert into users (Id, Email, Password, FirstName, LastName) values (null, ?, ?, ?, ?)";
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindValue(1, $user->getEmail(), PDO::PARAM_STR);
+        $stmt->bindValue(2, $user->getPassword(), PDO::PARAM_STR);
+        $stmt->bindValue(3, $user->getFirstName(), PDO::PARAM_STR);
+        $stmt->bindValue(4, $user->getLastName(), PDO::PARAM_STR);
+  
+        $stmt->execute();
+        
+        if($stmt->rowCount() == 1){
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+}
+
+?>
