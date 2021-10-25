39c37df266324815c2b63fe39a36242f0a1d3509
dtrowbri <dtrowbridg@my.gcu.edu>
2021-10-24 23:21:37 -0700

    Created Authentication DAO

--- database/authenticationdao.php
+++ database/authenticationdao.php
@@ -1,8 +1,25 @@
 <?php
 
+//require_once './database.php';
+
 class AuthenticationDAO {
     
-    
-}
+    function authenticate(?int $UserId, ?string $Password, $conn){
+        $query = "select count(Id) as `count` from users where Id = ? and Password = ?";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(1, $UserId, PDO::PARAM_INT);
+        $stmt->bindParam(2, $Password, PDO::PARAM_STR);
 
+        $stmt->execute();
+        
+        $result = $stmt->fetch();
+        
+        if($result['count'] == 1){
+            return true;
+        } else {
+            return false;
+        }
+    }
+       
+}
 ?>
