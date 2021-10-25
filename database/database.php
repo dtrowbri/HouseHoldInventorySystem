3fdf8e75bcef11927c148d144fdade05d1f4446d
dtrowbri <dtrowbridg@my.gcu.edu>
2021-10-24 19:59:31 -0700

    added database

---
+++ database/database.php
@@ -0,0 +1,20 @@
+<?php 
+
+class Database{
+
+    private $dsn = 'mysql:dbname=householdinventory;host=127.0.0.1;port=3306';
+    private $username = 'root';
+    private $password = 'root';
+    
+    function getConnection(){
+        $conn = new PDO($this->dsn, $this->username, $this->password);
+        
+        if($conn){
+            return $conn;
+        } else {
+            return null;
+        }
+    }
+}
+
+?>
