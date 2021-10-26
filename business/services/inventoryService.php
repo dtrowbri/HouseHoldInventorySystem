a02749e3f2b10e65583d3758780860a32105f4e4
dtrowbri <dtrowbridg@my.gcu.edu>
2021-10-25 23:02:33 -0700

    Created Inventory Item Service

--- business/services/inventoryService.php
+++ business/services/inventoryService.php
@@ -1,43 +1,43 @@
 <?php
 
+//require_once '../../database/database.php';
+//require_once '../../database/inventoryitemdao.php';
+//require_once '../models/inventoryitem.php';
+
 class InventoryService {
     
+    private $database;
+    
     function __construct(){
-        
+        $this->database = new Database();
     }
     
     function addInventoryItem(?InventoryItem $InventoryItem){
-        //This code is test code for the front-end user. Real code will be added later
-        if($InventoryItem->getDescription() != null && $InventoryItem->getHouseHoldId() != null && $InventoryItem->getName() != null && $InventoryItem->getQuantity() != null){
-            return true;
-        } else {
-            return false;
-        }
+        $conn = $this->database->getConnection();
+        $dao = new InventoryItemDAO();
+        $results = $dao->addInventoryItem($InventoryItem, $conn);
+        return $results;
     }
     
     function updateInventoryItem(?InventoryItem $InventoryItem){
-        //This code is test code for the front-end user. Real code will be added later
-        return true;
+        $conn = $this->database->getConnection();
+        $dao = new InventoryItemDAO();
+        $results = $dao->updateInventoryItem($InventoryItem, $conn);
+        return $results;
     }
     
-    function deleteInventoryItem(?InventoryItem $InventoryItem){
-        //This code is test code for the front-end user. Real code will be added later
-        return true;
+    function deleteInventoryItem(?int $InventoryItemId){
+        $conn = $this->database->getConnection();
+        $dao = new InventoryItemDAO();
+        $results = $dao->deleteInventoryItem($InventoryItemId, $conn);
+        return $results;
     }
     
-    function getHouseHoldInventoryItems(?int $HouseHold){
-        //This code is test code for the front-end user. Real code will be added later
-        $InventoryItems = array();
-        $InventoryItem1 = new InventoryItem("Sponge", "Sponge with brillo pad", 2, 1);
-        $InventoryItem1->setId(1);
-        $InventoryItem2 = new InventoryItem("Bleach", "Bottle of Bleach - 32oz", 1, 1);
-        $InventoryItem2->setId(2);
-        
-        array_push($InventoryItems, $InventoryItem1);
-        array_push($InventoryItems, $InventoryItem2);
-        
-        return $InventoryItems;
-        
+    function getHouseHoldInventoryItems(?int $HouseHoldId){
+        $conn = $this->database->getConnection();
+        $dao = new InventoryItemDAO();
+        $results = $dao->getInventoryItems($HouseHoldId, $conn);
+        return $results;
     }
 }
 
