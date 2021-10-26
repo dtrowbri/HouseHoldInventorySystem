278ddfb8b2cb870932aec67d02095603b2da8bb3
dtrowbri <dtrowbridg@my.gcu.edu>
2021-10-26 00:03:53 -0700

    Removed test function

--- business/services/authenticationService.php
+++ business/services/authenticationService.php
@@ -11,17 +11,6 @@ class AuthenticationService{
         $this->database = new Database();
     }
     
-    
-    function TestAuthenticate(?string $UserId, ?string $password){
-        
-        //This is test code for the front-end testers. Will be replacing with code in later stages
-        if($UserId == "test" && $password == "test"){
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
     function authenticate(?int $UserId, ?string $Password){
         $conn = $this->database->getConnection();
         $dao = new AuthenticationDAO();
