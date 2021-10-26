1201f7acb56bf6a5d4651e64d884dabc19da2e8a
dtrowbri <dtrowbridg@my.gcu.edu>
2021-10-25 23:26:36 -0700

    Added transactions to inventory service

--- business/services/inventoryService.php
+++ business/services/inventoryService.php
@@ -15,21 +15,47 @@ class InventoryService {
     function addInventoryItem(?InventoryItem $InventoryItem){
         $conn = $this->database->getConnection();
         $dao = new InventoryItemDAO();
+        
+        $conn->beginTransaction();
         $results = $dao->addInventoryItem($InventoryItem, $conn);
+        
+        if($results){
+            $conn->commit();
+        } else {
+            $conn->rollBack();
+        }
+        
         return $results;
     }
     
     function updateInventoryItem(?InventoryItem $InventoryItem){
         $conn = $this->database->getConnection();
         $dao = new InventoryItemDAO();
+        
+        $conn->beginTransaction();
         $results = $dao->updateInventoryItem($InventoryItem, $conn);
+        
+        if($results){
+            $conn->commit();
+        } else {
+            $conn->rollBack();
+        }
+        
         return $results;
     }
     
     function deleteInventoryItem(?int $InventoryItemId){
         $conn = $this->database->getConnection();
         $dao = new InventoryItemDAO();
+        
+        $conn->beginTransaction();
         $results = $dao->deleteInventoryItem($InventoryItemId, $conn);
+        
+        if($results){
+            $conn->commit();
+        }else {
+            $conn->rollBack();
+        }
         return $results;
     }
     
