cf4ae435eab3fbdac2cbed9aac195a88f8e624b5
dtrowbri <dtrowbridg@my.gcu.edu>
2021-10-30 11:23:06 -0700

    Footer sticks to the bottom now

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 </body>
-<footer class="text-center text-white">
+<footer class="text-center text-white fixed-bottom">
     <hr class="text-black-50" style="margin-bottom: 0em;">
     <div class="text-center p-3">
     	<span class="text-muted">Isaac Tucker & Donald Trowbridge | GCU | CST-323</span>
