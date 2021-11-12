64b72ce4f09b83e1f124676aca0171f0fd1ab3d6
dtrowbri <dtrowbridg@my.gcu.edu>
2021-11-11 21:18:11 -0700

    Added Monolog location to the autoloader

--- Autoloader.php
+++ Autoloader.php
@@ -4,4 +4,5 @@ spl_autoload_register(function($class){
     require_once 'business/models/' . $class . '.php';
     require_once 'business/services/' . $class . '.php';
 });
+    require_once __DIR__ .'/vendor/autoload.php';
 ?>
