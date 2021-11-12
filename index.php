03d2612eabdde76e87086e51fa0d4b03e79e45ae
Isaac Tucker <isaac.takka@gmail.com>
2021-11-11 21:09:45 -0700

    Monolog

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
 session_start();
-require_once 'header.php'; 
+require_once 'header.php';
+require_once 'Autoloader.php';
 echo $message;
-?>	
+
+require __DIR__ . '/vendor/autoload.php';
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$logger = new Logger('testlogger');
+
+$logger->pushHandler(new StreamHandler(__DIR__ . '/app.log', Logger::DEBUG));
+$logger->info("Welcome to the HouseHoldInventorySystem index page!",[null]);
+
+?>
 	<div class="container">
 		<div class="row mt-3 text-center">
 			<h1>Welcome to the CST-323 HouseHold Inventory System!</h1>
