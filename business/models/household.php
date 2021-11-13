d622dd1e1c796b6216585b82de6b2413a0512222
Isaac Tucker <isaac.takka@gmail.com>
2021-11-12 21:04:40 -0700

    Update household.php

--- business/models/household.php
+++ business/models/household.php
@@ -1,46 +1,122 @@
 <?php
 
+/**
+ * HouseHold
+ * object to encapsulate household information.
+ */
 class HouseHold{
-    
-    private $Id;
-    private $Name;
-    private $Address;
+        
+    /**
+     * Id
+     *
+     * @var int
+     */
+    private $Id;    
+    /**
+     * Name
+     *
+     * @var string
+     */
+    private $Name;    
+    /**
+     * Address
+     *
+     * @var string
+     */
+    private $Address;    
+    /**
+     * UserId
+     *
+     * @var int
+     */
     private $UserId;
-    
+        
+    /**
+     * __construct
+     *
+     * @param  string $Name
+     * @param  string $Address
+     * @param  int $UserId
+     * @return void
+     */
     function __construct(?string $Name, ?string $Address, ?int $UserId){
         $this->setName($Name);
         $this->setAddress($Address);
         $this->setUserId($UserId);
     }
-    
+        
+    /**
+     * getId
+     *
+     * @return int
+     */
     function getId(){
         return $this->Id;
     }
-    
+        
+    /**
+     * setId
+     *
+     * @param  int $Id
+     * @return void
+     */
     function setId(?int $Id){
         $this->Id = $Id;
     }
-    
+        
+    /**
+     * getName
+     *
+     * @return string
+     */
     function getName(){
         return $this->Name;
     }
-    
+        
+    /**
+     * setName
+     *
+     * @param  string $Name
+     * @return void
+     */
     function setName(?string $Name){
         $this->Name = $Name;
     }
-    
+        
+    /**
+     * getAddress
+     *
+     * @return string
+     */
     function getAddress(){
         return $this->Address;
     }
-    
+        
+    /**
+     * setAddress
+     *
+     * @param  string $Address
+     * @return void
+     */
     function setAddress(?string $Address){
         $this->Address = $Address;
     }
-    
+        
+    /**
+     * getUserId
+     *
+     * @return int
+     */
     function getUserId(){
         return $this->UserId;
     }
-    
+        
+    /**
+     * setUserId
+     *
+     * @param  int $UserId
+     * @return void
+     */
     function setUserId(?int $UserId){
         $this->UserId = $UserId;
     }
